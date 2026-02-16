1cecbef40dbe076d81399530b53dd24d5c150f0b
vinit <vinitk81144@gmail.com>
2026-02-16 22:59:29 +0530

    added graph memory

--- memory_agent/memory.py
+++ memory_agent/memory.py
@@ -10,6 +10,18 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 if not gemini_api_key:
     raise RuntimeError("GEMINI_API_KEY not set")
 
+neo_connection_uri = os.getenv("NEO_CONNECTION_URI")
+if not neo_connection_uri:
+    raise RuntimeError("NEO_CONNECTION_URI not set")
+
+neo_username = os.getenv("NEO_USERNAME")
+if not neo_username:
+    raise RuntimeError("NEO_USERNAME not set")
+
+neo_password = os.getenv("NEO_PASSWORD")
+if not neo_password:
+    raise RuntimeError("NEO_PASSWORD not set")
+
 client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
     api_key=gemini_api_key,
@@ -30,6 +42,14 @@ config = {
         "config": {
             "api_key": gemini_api_key,
             "model": "gemini/2.5-flash",
+        },
+    },
+    "graph_store": {
+        "provider": "neo4j",
+        "config": {
+            "url": neo_connection_uri,
+            "username": neo_username,
+            "password": neo_password,
         },
     },
     "vector_store": {
@@ -77,4 +97,3 @@ memory_client.add(
 )
 
 print("Memory has been saved..")
-
